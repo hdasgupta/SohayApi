c719c98599128c825ca96a8838209c667bfc71a6
Himaghna Dasgupta <himaghna.dasgupta@gmail.com>
2025-09-22 22:47:41 +0530

    Introduce Environment Variable

--- src/main/java/com/wbfmh/sohay/SohayApplication.java
+++ src/main/java/com/wbfmh/sohay/SohayApplication.java
@@ -1,5 +1,6 @@
 package com.wbfmh.sohay;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SohayApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // This loads the .env file
+		dotenv.entries().forEach(entry -> {
+					if (System.getenv(entry.getKey()) == null)
+						System.setProperty(entry.getKey(), entry.getValue());
+				});
+
 		SpringApplication.run(SohayApplication.class, args);
 	}
 
